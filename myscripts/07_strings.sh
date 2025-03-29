11af256c529e3f32f6236409c371612385f55974
harshil114 <harshilvispute114@gmail.com>
2025-03-29 16:56:36 +0530

    strings

---
+++ myscripts/07_strings.sh
@@ -0,0 +1,16 @@
+str="hello how are you?"
+
+echo ${str}
+
+echo "length: ${#str}"
+
+echo "upper case: ${str^^}"
+
+
+echo "lower case: ${str,,}"
+
+
+echo "replace: ${str/hi/hello}"
+
+echo "slice: ${str:6:11}"
+
